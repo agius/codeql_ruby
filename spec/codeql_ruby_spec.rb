d930d628a37dd36843180c24e97c19d9b9dd75bf
agius <andrew@atevans.com>
2020-09-04 15:52:07 -0400

    Move codeql query tests to use codeql cli runner

--- spec/codeql_ruby_spec.rb
+++ spec/codeql_ruby_spec.rb
@@ -5,32 +5,11 @@ RSpec.describe CodeqlRuby do
     expect(CodeqlRuby::VERSION).not_to be nil
   end
 
-  it "extracts a db, runs a query, and generates JSON results" do
-    results = CodeqlRunner.results_for_db('base_unsafe_script')
-    tuples = results.dig('#select', 'tuples')
-
-    expect(tuples).to include(['eval', 'This is a leaf node.'])
-  end
-
-  it "extracts a directory as a db and queries it" do
-    results = CodeqlRunner.results_for_db('script_with_require')
-    tuples = results.dig('#select', 'tuples')
-
-    expect(tuples).to include(['RequiredFile', 'This is a leaf node.'])
-  end
-
   it "extracts a file to relevant trap structures" do
-    filepath = File.expand_path(File.join(File.dirname(__FILE__), 'script_with_require', 'script_with_require.rb'))
+    filepath = File.expand_path(File.join(File.dirname(__FILE__), '..', 'ql', 'test', 'script_with_require', 'script_with_require.rb'))
     ef = CodeqlRuby::ExtractorFile.new(filepath)
     results = ef.to_trap
 
     expect(results).to be_a(String)
-  end
-
-  it "extracts Location info from LeafNodes" do
-    results = CodeqlRunner.results_for_db('leaf_node_location')
-    tuples = results.dig('#select', 'tuples')
-
-    expect(tuples).to include([{'label'=>'LeafNode'}, 'puts', 'leaf_node_location.rb:1'])
   end
 end
