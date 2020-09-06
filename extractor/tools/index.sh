9d201c8d417659f1a3e75f86d21a155881c0a23c
agius <andrew@atevans.com>
2020-09-06 16:00:31 -0400

    Fix test extractor runner maybe

--- extractor/tools/index.sh
+++ extractor/tools/index.sh
@@ -12,14 +12,4 @@
 
 set -eu
 
-echo "running index.sh"
-echo "CODEQL_PLATFORM: $CODEQL_PLATFORM"
-echo "CODEQL_EXTRACTOR_RUBY_ROOT: $CODEQL_EXTRACTOR_RUBY_ROOT"
-ls -lha "$CODEQL_EXTRACTOR_RUBY_ROOT/tools/$CODEQL_PLATFORM/"
-
-if [ "$CODEQL_PLATFORM" != "linux64" ] && [ "$CODEQL_PLATFORM" != "osx64" ] ; then
-    echo "Automatic build detection for $CODEQL_PLATFORM is not implemented."
-    exit 1
-fi
-
-"$CODEQL_EXTRACTOR_RUBY_ROOT/tools/$CODEQL_PLATFORM/ruby-autobuilder"
+codeql_ruby $@
